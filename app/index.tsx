b8278f696185e57cdaa22b00072fbecfe8a323a6
Gustasilvadev <gustamarsil@gmail.com>
2025-09-01 09:52:37 -0300

    Atualização na estilização

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Ola Mundo</Text>
+      <Image source={require('./foco.png')} />
+
+      <View style={styles.actions}>
+        <Text style={styles.timer}>25:00</Text>
+
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </Pressable>
+
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}></Text>
+        Projeto ficticio e sem fins comerciais. Desenvolvido por Aluno
+
+      </View>
+
     </View>
   );
 }
@@ -14,8 +29,47 @@ const styles = StyleSheet.create({
     flex:1,
     justifyContent: "center",
     alignItems:"center",
-    backgroundColor: "lightgreen"
+    backgroundColor: '#021123'
+  },
+
+  actions:{
+    padding:24,
+    backgroundColor:'#144480',
+    width:'80%',
+    height:'30%',
+    borderColor:'#144480',
+    borderRadius:32,
+    borderWidth:2,
+    alignItems:"center",
+    justifyContent:"center"
+
+  },
+
+   timer:{
+    color: '#ffffff',
+    fontSize:53,
+    fontFamily:"Unbounded",
+    fontWeight:600
+
+  },
+
+
+   button:{
+
+  },
+
+  buttonText:{
+
+  },
+
+  footer:{
+
+  },
+
+  footerText:{
+
   }
+
 
 })
 
@@ -26,3 +80,4 @@ const styles = StyleSheet.create({
 //         alignItems: "center",
 //         backgroundColor: 'blue',
 //       }}
+//<Image source={require('./foco.png')} />
--- documentacao.md
+++ documentacao.md
@@ -1,14 +1,23 @@
 # COMANDOS DO REACT NATIVE
 
-npx create-expo-app@3 fokus
--> Criar um app react native com o nome fokus, na versao 3.
+1. npx create-expo-app@3 fokus --> Criar um app react native com o nome fokus, na versao 3.
 
-npm start
---> Prepara servidor para sr executadonpm start
+2. npm start --> Prepara servidor para sr executadonpm start.
 
-npm run reset-project
---> Reseta o projeto para ficar limpo
+3. npm run reset-project --> Reseta o projeto para ficar limpo.
+
+4. npm run web --> Inicializa o app no naveador.
+
+5. npm install --> Instala todas a dependencias necessarias.
 
 ## COMPONENTES DO REACT NATIVE
-<view> </view> --> Funciona como container "div". É obrigatorio para outros componentes
+<view> </view> --> Funciona como container "div". É obrigatorio para outros componentes. É por padrão flex.
 <Text> </Text> --> Funciona quando você vi utilizar textos.
+<Image> --> Usado para renderizar imagens no projeto.
+<Pressable></Pressable> --> Utilizado quando temos algo pressionavel.
+
+## FUNÇÕES E CLASSES UTILIZADAS NO REACT NATIVE
+
+1. require() --> Faz com que voce consiga passar um caminho de imagem que esta dentro do projeto.
+2. create() --> Utilizada qunado vc quer criar um campo de estilização.
+3. Stylesheet --> Utilizada para criar um campo de estilização.
